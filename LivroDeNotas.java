7459e2bb0428491bd09a83022fca081a5cf4f130
Rodrigo Bossini <professorbossini@gmail.com>
2022-09-06 20:17:27 -0300

    apostila05: passo 4 encerrado

---
+++ LivroDeNotas.java
@@ -0,0 +1,8 @@
+public class LivroDeNotas {
+
+  //método é um bloco de código com nome
+  public void exibirMensagem(){
+    System.out.println("Bem vindo ao livro de notas"); 
+  }
+
+}
---
+++ TesteLivroDeNotas.java
@@ -0,0 +1,10 @@
+public class TesteLivroDeNotas {
+  public static void main(String[] args) {
+    //operador de construção de objetos: new
+    //variável de referência
+    LivroDeNotas livro;
+    livro = new LivroDeNotas();
+    //operador de acesso a membro: .
+    livro.exibirMensagem();
+  }
+}
